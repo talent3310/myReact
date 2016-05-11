2cb4cb859ac72c01a796368eade6853ec6a32a53
deniz <deniz.aslan331@hotmail.com>
2016-05-10 22:35:20 -0700

    fixed

--- bundle.js
+++ bundle.js
@@ -42,8 +42,9 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
+	 __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./style.css\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
 	document.write("It works.");
 
 /***/ }
--- entry.js
+++ entry.js
@@ -1,1 +1,2 @@
+ require("./style.css");
 document.write("It works.");
